5306460bc67c8e663da1920fd187e97b61feeadb
root <nbarcalo@uccs.edu>
2021-04-28 10:54:52 +0000

    string test

--- spec/FirstExample.rb
+++ spec/FirstExample.rb
@@ -5,3 +5,12 @@ RSpec.describe "The math blow is wrong" do
 		expect(6 * 7).to eq(43)
 	end
 end
+
+RSpec.describe "hello spec" do
+	describe String do
+		let(:string) { String.new }
+		it "should provide an empty string" do
+			expect(string).to eq("")
+		end
+	end
+end
